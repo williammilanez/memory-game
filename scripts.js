d029f6ee5b20086c7fd14d771c3e3d46d2edbb03
William Milanez <william.milanez@outlook.com>
2025-06-05 21:28:05 -0300

    embaralhar as cartas

--- scripts.js
+++ scripts.js
@@ -6,20 +6,29 @@ let isCheckingPair = false; // Trava o jogo enquanto verifica o par ou esconde a
 
 // Array com todas as cartas do jogo
 const cardItems = [
-  { id: 1, content: "👻", matched: false },
-  { id: 2, content: "👻", matched: false },
-  { id: 3, content: "☠️", matched: false },
-  { id: 4, content: "☠️", matched: false },
-  { id: 5, content: "👽", matched: false },
-  { id: 6, content: "👽", matched: false },
-  { id: 7, content: "👾", matched: false },
-  { id: 8, content: "👾", matched: false },
-  { id: 9, content: "🎃", matched: false },
-  { id: 10, content: "🎃", matched: false },
-  { id: 11, content: "🤡", matched: false },
-  { id: 12, content: "🤡", matched: false },
-  { id: 13, content: "💩", matched: false },
-  { id: 14, content: "💩", matched: false },
-  { id: 15, content: "🤖", matched: false },
-  { id: 16, content: "🤖", matched: false },
+  { id: 1, content: "🤡", matched: false },
+  { id: 2, content: "🤡", matched: false },
+  { id: 3, content: "👻", matched: false },
+  { id: 4, content: "👻", matched: false },
+  { id: 5, content: "☠️", matched: false },
+  { id: 6, content: "☠️", matched: false },
+  { id: 7, content: "👽", matched: false },
+  { id: 8, content: "👽", matched: false },
+  { id: 9, content: "👾", matched: false },
+  { id: 10, content: "👾", matched: false },
+  { id: 11, content: "🤖", matched: false },
+  { id: 12, content: "🤖", matched: false },
+  { id: 13, content: "🎃", matched: false },
+  { id: 14, content: "🎃", matched: false },
+  { id: 15, content: "😻", matched: false },
+  { id: 16, content: "😻", matched: false },
 ];
+
+function shuffleCards(array) {
+  const shuffled = array.sort(() => (Math.random() > 0.5 ? 1 : -1)); // Positivo vai depois, negativo vai antes
+
+  return shuffled;
+}
+
+const cards = shuffleCards(cardItems);
+console.log(cards);
